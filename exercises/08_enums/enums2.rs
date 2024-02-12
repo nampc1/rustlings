420ae85dfdeb279271858d799a40842a8039cb27
nampc <namphamc99@gmail.com>
2024-02-13 01:13:32 +0700

    up to chap 10

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -3,11 +3,12 @@
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
-    // TODO: define the different variants used below
+    Move { x: i32, y: i32 },
+    Echo(String),
+    ChangeColor(i32, i32, i32),
+    Quit
 }
 
 impl Message {
@@ -25,6 +26,6 @@ fn main() {
     ];
 
     for message in &messages {
-        message.call();
+        message.call(());
     }
 }
