e6c7186e2bcd831f1648f048a7b308fcedb02c7b
nampc <namphamc99@gmail.com>
2024-03-07 00:26:11 +0700

    conversions

--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,40 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let mut s_iter = s.split(',');
+        let Some(name) = s_iter.next() else {
+            return Err(ParsePersonError::NoName);
+        };
+
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let Some(age_str) = s_iter.next() else {
+            return Err(ParsePersonError::BadLen);
+        };
+
+        let age = age_str.parse::<usize>().map_err(|err| {
+            ParsePersonError::ParseInt(err)
+        })?;
+
+        if let Some(extra_commas) = s_iter.next() {
+            return Err(ParsePersonError::BadLen)
+        }
+
+        Ok(Person {
+            name: name.to_string(),
+            age
+        })
+
+        // match age {
+        //     Some(Ok(num)) => ,
+        //     _ => Err(ParsePersonError::BadLen)
+        // }
     }
 }
 
